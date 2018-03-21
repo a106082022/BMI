3c7a607408236f1bc018cca09e0a42b537d85ff9
Michael <A106082022@mail.shu.edu.tw>
2018-03-22 00:34:55 +0800

    雛型完成

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+            Console.Write("請輸入體重(KG):");
+            double KG = double.Parse(Console.ReadLine());  //讓程式知道體重
+            Console.Write("請輸入身高(CM):");              
+            double CM = double.Parse(Console.ReadLine());  //讓程式知道身高
+            double BMI = KG / (CM/100 * CM/100);           //讓程式算BMI
+            Console.WriteLine("BMI值為"+BMI);              //讓程告訴我BMI
+            Console.Read();
         }
     }
 }
