cd47670cb684ea14889009ae55aa87accf1e8f91
Michael <A106082022@mail.shu.edu.tw>
2018-03-22 03:35:27 +0800

    改善註解

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,7 +16,9 @@ namespace ConsoleApp1
             double KG = double.Parse(Console.ReadLine());  //讓程式知道體重
             double BMI = KG / (CM/100 * CM/100);           //讓程式算BMI
             Console.WriteLine("BMI值為"+BMI);              //讓程告訴我BMI
-            //類型區別留空
+            //KG就是體重，CM就是身高
+            //BMI就是BMI，CM/100是把公分轉換至公尺 
+            //-----------------------我是分隔線-------------------------
             if (BMI < 18.5)                                //判斷BMI
                 Console.WriteLine("體重過輕");
             else if (18.5 <= BMI && BMI < 24)
@@ -27,7 +29,7 @@ namespace ConsoleApp1
                 Console.WriteLine("肥胖");
             else
                 Console.WriteLine("請檢察輸入數值");        //BMI判斷結束
-            //類型區別留空
+            //-----------------------我是分隔線-------------------------
             if (CM >= 196 && BMI > 31.5)                    //判斷兵役體位
                 Console.WriteLine("免役(身高和BMI均過高)");
             else if (CM >= 196 && BMI < 16.5)
@@ -36,7 +38,7 @@ namespace ConsoleApp1
                 Console.WriteLine("免役(身高和BMI均過低)");
             else if (CM <= 157 && BMI > 31.5)
                 Console.WriteLine("免役(身高過低且BMI過高)");
-            //類型區別留空
+            //-----------------------我是分隔線-------------------------
             else if (BMI < 16.5)                                
                 Console.WriteLine("免役(BMI過低)");
             else if (BMI > 31.5)
@@ -45,7 +47,7 @@ namespace ConsoleApp1
                 Console.WriteLine("免役(身高過低)");
             else if (CM >= 196)
                 Console.WriteLine("免役(身高過高)");
-            //類型區別留空
+            //-----------------------我是分隔線-------------------------
             else if (17 <= BMI && BMI <= 31)
                 Console.WriteLine("常備役");
             else if (31 < BMI && BMI <= 31.5)
