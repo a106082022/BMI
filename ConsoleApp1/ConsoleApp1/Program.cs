f4102fd3d669614e1e82d5e4182672220adef2a6
Michael <A106082022@mail.shu.edu.tw>
2018-03-22 03:25:06 +0800

    結果資訊增加 排版改良 註解改良

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,7 +16,7 @@ namespace ConsoleApp1
             double KG = double.Parse(Console.ReadLine());  //讓程式知道體重
             double BMI = KG / (CM/100 * CM/100);           //讓程式算BMI
             Console.WriteLine("BMI值為"+BMI);              //讓程告訴我BMI
-
+            //類型區別留空
             if (BMI < 18.5)                                //判斷BMI
                 Console.WriteLine("體重過輕");
             else if (18.5 <= BMI && BMI < 24)
@@ -26,15 +26,26 @@ namespace ConsoleApp1
             else if (28 <= BMI)
                 Console.WriteLine("肥胖");
             else
-                Console.WriteLine("請檢察輸入數值");
-            if (BMI < 16.5)
-                Console.WriteLine("免役");
+                Console.WriteLine("請檢察輸入數值");        //BMI判斷結束
+            //類型區別留空
+            if (CM >= 196 && BMI > 31.5)                    //判斷兵役體位
+                Console.WriteLine("免役(身高和BMI均過高)");
+            else if (CM >= 196 && BMI < 16.5)
+                Console.WriteLine("免役(身高過高且BMI過低)");
+            else if (CM <= 157 && BMI < 16.5)
+                Console.WriteLine("免役(身高和BMI均過低)");
+            else if (CM <= 157 && BMI > 31.5)
+                Console.WriteLine("免役(身高過低且BMI過高)");
+            //類型區別留空
+            else if (BMI < 16.5)                                
+                Console.WriteLine("免役(BMI過低)");
             else if (BMI > 31.5)
-                Console.WriteLine("免役");
+                Console.WriteLine("免役(BMI過高)");
             else if (CM <= 157)
-                Console.WriteLine("免役");
+                Console.WriteLine("免役(身高過低)");
             else if (CM >= 196)
-                Console.WriteLine("免役");
+                Console.WriteLine("免役(身高過高)");
+            //類型區別留空
             else if (17 <= BMI && BMI <= 31)
                 Console.WriteLine("常備役");
             else if (31 < BMI && BMI <= 31.5)
@@ -43,7 +54,10 @@ namespace ConsoleApp1
                 Console.WriteLine("替代役");
             else
                 Console.WriteLine("請檢察輸入數值");
-            Console.Read();
+            Console.Read();                               //兵役體位判斷結束
+            //程式結束
+
+           
         }
     }
 }
